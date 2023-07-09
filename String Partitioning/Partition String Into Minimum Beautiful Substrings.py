e88e16849ea698451000282d15159d03f4f1654d
Shaik Iftequar Ahmed <iftequar.154@gmail.com>
2023-07-09 22:08:31 +0530

    Create Partition String Into Minimum Beautiful Substrings.py

---
+++ String Partitioning/Partition String Into Minimum Beautiful Substrings.py
@@ -0,0 +1,44 @@
+### Link: https://leetcode.com/problems/partition-string-into-minimum-beautiful-substrings/
+
+class Solution:
+    def minimumBeautifulSubstrings(self, s: str) -> int:
+        
+        def isValid(s):
+            
+            if(s[0]=='0'):
+                return False
+            
+            ans=0
+            for i in range(len(s)-1,-1,-1):
+                if(s[i]=='1'):
+                    ans=ans+2**(len(s)-i-1)
+            # print("Decimal: ",ans)
+            while(True):
+                if(ans==1):
+                    break
+                if(ans%5!=0):
+                    return False
+                else:
+                    ans=ans/5
+            
+            return True
+            
+              
+        def recursion(s):
+            
+            if(len(s)==0):
+                return 0
+            
+            ans=20
+            for i in range(len(s)):
+                firstString=s[:i+1]
+                secondString=s[i+1:]
+                if(isValid(firstString)):
+                    ans = min(ans,recursion(secondString)+1)
+            
+            return ans
+            
+        # print(isPowerOf5('1011'))
+        if(recursion(s)==20):
+            return -1
+        return recursion(s) 
